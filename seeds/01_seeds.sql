80f5ab9c9d990269246bb2488b6f0edc66b5dac6
Thomas Ngo <thomasngo818@gmail.com>
2022-02-10 05:29:26 +0000

    Add statement to insert seed data for users table

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (name, email, password)
+VALUES ('Alice Apple', 'aaple@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Bob Bell', 'bigbob@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Carol Chrysler', 'carolc@example.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
