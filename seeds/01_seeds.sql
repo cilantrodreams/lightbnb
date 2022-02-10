8998a52358c70e209ffe5e32bccf0dc35b79e796
Thomas Ngo <thomasngo818@gmail.com>
2022-02-10 05:35:43 +0000

    Add statement to insert seed data for property reviews table

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -74,3 +74,8 @@ INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2018-09-11', '2018-09-26', 1, 2),
 ('2019-01-04', '2019-02-01', 2, 3),
 ('2021-10-01', '2021-10-14', 3, 2);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (2, 1, 1, 3, 'messages'),
+(3, 2, 2, 5, 'messages'),
+(2, 3, 3, 1, 'messages');
