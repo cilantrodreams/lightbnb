7bc8ad602296733ee4eef828546dd462a922b0a7
Thomas Ngo <thomasngo818@gmail.com>
2022-02-10 05:33:08 +0000

    Add statement to insert seed data fpr reservations table

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -69,3 +69,8 @@ VALUES (
   'A1A 1A1',
   false
 );
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 2),
+('2019-01-04', '2019-02-01', 2, 3),
+('2021-10-01', '2021-10-14', 3, 2);
